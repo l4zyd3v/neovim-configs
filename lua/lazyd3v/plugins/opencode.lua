1fd03cb48789d55be94a68238a1362a980cef529
l4zyd3v <nielsen.177@gmail.com>
2026-02-25 00:23:34 +0100

    opencode added

--- lua/lazyd3v/plugins/opencode.lua
+++ lua/lazyd3v/plugins/opencode.lua
@@ -6,11 +6,36 @@ return {
     ---@module 'snacks' <- Loads `snacks.nvim` types for configuration intellisense.
     { "folke/snacks.nvim", opts = { input = {}, picker = {}, terminal = {} } },
   },
+
   config = function()
     ---@type opencode.Opts
     vim.g.opencode_opts = {
-      -- Your configuration, if any — see `lua/opencode/config.lua`, or "goto definition" on the type or field.
+      provider = {
+        enabled = "snacks",
+        snacks = {
+          win = {
+            position = "float",
+            width = 0.85,
+            height = 0.85,
+            border = "rounded",
+            enter = true,
+          },
+        },
+      },
     }
+
+    -- experimental - from opencode
+    local opencode_group = vim.api.nvim_create_augroup("lazyd3v_opencode", { clear = true })
+    vim.api.nvim_create_autocmd("FileType", {
+      group = opencode_group,
+      pattern = "opencode_terminal",
+      callback = function(event)
+        vim.keymap.set("t", "<C-c>", function()
+          require("opencode").toggle()
+        end, { buffer = event.buf, desc = "Hide opencode window" })
+      end,
+    })
+    -- experimental - from opencode ^^^^^^
 
     -- Required for `opts.events.reload`.
     vim.o.autoread = true
@@ -22,7 +47,7 @@ return {
     vim.keymap.set({ "n", "x" }, "<C-x>", function()
       require("opencode").select()
     end, { desc = "Execute opencode action…" })
-    vim.keymap.set({ "n", "t" }, "<C-.>", function()
+    vim.keymap.set({ "n", "t" }, "<Leader>jj", function()
       require("opencode").toggle()
     end, { desc = "Toggle opencode" })
 
